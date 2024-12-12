c8fe6a5a8f3566749e20c41b4390e4ac5c32d6a5
Vladimir <kremench@gmail.com>
2024-12-12 10:33:38 +0200

    adds files js

---
+++ src/js/swiper.js
@@ -0,0 +1,14 @@
+new Swiper('.swiper', {
+  loop: true,
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1280: {
+      slidesPerView: 3,
+    },
+  },
+});
