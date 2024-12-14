57f25f8aa8638f61ba5a57dd8b92c25c2e6cf183
Vladimir <kremench@gmail.com>
2024-12-14 23:16:24 +0200

    add click burger menu

--- src/js/modal.js
+++ src/js/modal.js
@@ -4,10 +4,12 @@
     openModalBtn: document.querySelector('[data-modal-open]'),
     // Додати атрибут data-modal-close на кнопку закриття
     closeModalBtn: document.querySelector('[data-modal-close]'),
+    //додаем слухачі на кнопки вибору бургер меню
+    choiceSectionMenu: document.querySelectorAll('burger-link'),
     // Додати атрибут data-modal на бекдроп модалки
     modal: document.querySelector('[data-modal]'),
   };
-
+  //   refs.choiceSectionMenu.addEventListener('click', toggleModal);
   refs.openModalBtn.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
 
